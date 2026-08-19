a9268ca73446275d43dd92090bd88db8d22d1987
taskheeragency-hue <taskheeragency@gmail.com>
2026-08-20 03:15:35 +0500

    Update cached asset from Taskheer_Agency_Estimator to index.html

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = "taskheer-estimator-v1";
 const ASSETS = [
-  "./Taskheer_Agency_Estimator.html",
+  "./index.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
